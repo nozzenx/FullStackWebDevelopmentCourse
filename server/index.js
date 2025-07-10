74e2d14c6c0fc01e847f89903c541ed22c1df8d5
Burak Bisneli <bisneliburak@gmail.com>
2025-07-10 20:43:01 +0300

    Tutorial - 2
    - used router for routing between pages
    
    - used get for getting all the posts in the database
    
    - used post for adding post to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+
 const db = require("./models"); //Getting the tables inside models folder.
 
+//Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {      //Syncing the database first then starting the server.
     app.listen(3001, () => {
